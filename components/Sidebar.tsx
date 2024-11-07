40d162b9cc58553f1202c4ab3c3aabb8f67721e9
joe95-ux <ogorktabi@gmail.com>
2024-11-07 22:12:31 +0200

    add sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import {CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon} from "lucide-react";
+
+const routes = [
+    {
+        href:"",
+        label:"Home",
+        icon: HomeIcon
+     
+    },
+    {
+        href:"workflows",
+        label:"Workflows",
+        icon: Layers2Icon
+     
+    },
+    {
+        href:"credentials",
+        label:"Credentials",
+        icon: ShieldCheckIcon
+     
+    },
+    {
+        href:"billing",
+        label:"Billing",
+        icon: CoinsIcon
+     
+    }
+]
 
 const DesktopSidebar = () => {
+ 
   return (
-    <div></div>
+    <div className='hidden relative md:block min-w-[280px]
+     max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 
+     dark:text-foreground text-muted-foreground border-r-2 border-separate'>
+        
+
+    </div>
   )
 }
 
