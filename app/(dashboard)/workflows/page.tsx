d4d380471b1b18100721515727cbc90a1798754d
joe95-ux <ogorktabi@gmail.com>
2024-11-16 23:07:38 +0200

    add workflows page

---
+++ app/(dashboard)/workflows/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Workflows = () => {
+  return (
+    <div className='flex-1 flex flex-col h-full'>
+        <div className="flex justify-betwwen">
+            <div className="flex flex-col">
+                <div className="text-3xl font-bold">Workflows</div>
+                <p className="text-muted-foreground">
+                    Manage your Workflows
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Workflows
