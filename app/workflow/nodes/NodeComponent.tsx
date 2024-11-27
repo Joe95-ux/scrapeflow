a4621581621871c548274a0e6847b21d7e4de886
joe95-ux <ogorktabi@gmail.com>
2024-11-27 22:34:15 +0200

    add input to Nodecomponent

--- app/workflow/nodes/NodeComponent.tsx
+++ app/workflow/nodes/NodeComponent.tsx
@@ -3,12 +3,20 @@ import { NodeProps } from "@xyflow/react";
 import NodeCard from "./NodeCard";
 import NodeHeader from "./NodeHeader";
 import { AppNodeData } from "@/types/appNode";
+import { TaskRegistry } from "@/lib/workflow/task/Registry";
+import { NodeInput, NodeInputs } from "./NodeInputs";
 
 const NodeComponet = memo((props: NodeProps) => {
   const nodeData = props.data as AppNodeData;
+  const task = TaskRegistry[nodeData.type];
   return (
     <NodeCard nodeId={props.id} isSelected={!!props.selected}>
       <NodeHeader taskType={nodeData.type}/>
+      <NodeInputs>
+        {task.inputs.map((input) =>(
+          <NodeInput key={input.name} input={input}/>
+        ))}
+      </NodeInputs>
     </NodeCard>
   );
 });
