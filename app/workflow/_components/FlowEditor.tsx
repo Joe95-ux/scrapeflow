9e0fd57cd9f182c6e914e0000d261f1d7b35dd20
joe95-ux <ogorktabi@gmail.com>
2024-12-06 19:55:04 +0200

    prevent cycles in workflows

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -8,6 +8,7 @@ import {
   Connection,
   Controls,
   Edge,
+  getOutgoers,
   ReactFlow,
   useEdgesState,
   useNodesState,
@@ -20,6 +21,7 @@ import { TaskType } from "@/types/task";
 import NodeComponet from "./nodes/NodeComponent";
 import { AppNode } from "@/types/appNode";
 import DeletableEdge from "./edges/DeletableEdge";
+import { TaskRegistry } from "@/lib/workflow/task/Registry";
 
 //tell floweditor what node component to use
 const nodeTypes = {
@@ -58,8 +60,7 @@ function FlowEditor({ workflow }: { workflow: Workflow }) {
   const onDrop = useCallback(
     (event: React.DragEvent) => {
       event.preventDefault();
-      const taskType = event.dataTransfer.getData
-      ("application/reactflow");
+      const taskType = event.dataTransfer.getData("application/reactflow");
       if (typeof taskType === undefined || !taskType) return;
       const position = screenToFlowPosition({
         x: event.clientX,
@@ -82,13 +83,61 @@ function FlowEditor({ workflow }: { workflow: Workflow }) {
       if (!node) return;
       const nodeInputs = node.data.inputs;
       updateNodeData(node.id, {
-        inputs:{
+        inputs: {
           ...nodeInputs,
-          [connection.targetHandle]: ""
-        }
+          [connection.targetHandle]: "",
+        },
       });
     },
     [nodes, setEdges, updateNodeData]
+  );
+
+  const isValidConnection = useCallback(
+    (connection: Edge | Connection) => {
+      // No self connection
+      if (connection.source === connection.target) {
+        return false;
+      }
+
+      // Same taskParam type connection
+      const source = nodes.find((node) => node.id === connection.source);
+      const target = nodes.find((node) => node.id === connection.target);
+      if (!source || !target) {
+        console.error("Invalid connection: Source or Target node not found");
+        return false;
+      }
+
+      const sourceTask = TaskRegistry[source.data.type];
+      const targetTask = TaskRegistry[target.data.type];
+
+      const output = sourceTask.outputs.find(
+        (o) => o.name === connection.sourceHandle
+      );
+
+      const input = targetTask.inputs.find(
+        (i) => i.name === connection.targetHandle
+      );
+
+      if (input?.type !== output?.type) {
+        console.error("invalid connection type: type mismatch");
+        return false;
+      }
+
+      // prevent looping
+      const hasCycle = (node: AppNode, visited = new Set()) => {
+        if (visited.has(node.id)) return false;
+        visited.add(node.id);
+
+        for (const outgoer of getOutgoers(node, nodes, edges)) {
+          if (outgoer.id === connection.source) return true;
+          if (hasCycle(outgoer, visited)) return true;
+        }
+      };
+
+      const detectedCycle = hasCycle(target);
+      return !detectedCycle;
+    },
+    [edges, nodes]
   );
 
   return (
@@ -107,6 +156,7 @@ function FlowEditor({ workflow }: { workflow: Workflow }) {
         onDragOver={onDragOver}
         onDrop={onDrop}
         onConnect={onConnect}
+        isValidConnection={isValidConnection}
       >
         <Controls position="top-left" fitViewOptions={fitViewOptions} />
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
