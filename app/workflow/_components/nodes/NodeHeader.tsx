e90bac30b1c0534e61567b80280bfebf6a9e39c1
joe95-ux <ogorktabi@gmail.com>
2024-12-08 20:21:52 +0200

    improve nodecard duplication

--- app/workflow/_components/nodes/NodeHeader.tsx
+++ app/workflow/_components/nodes/NodeHeader.tsx
@@ -36,7 +36,7 @@ function NodeHeader({ taskType, nodeId }: { taskType: TaskType, nodeId: string }
             <Button variant={"ghost"} size={"icon"} onClick={()=>{
               const node = getNode(nodeId) as AppNode;
               const newX = node.position.x;
-              const newY = node.position.y;
+              const newY = node.position.y + node.measured?.height! + 20;
               const newNode = CreateFlowNode(node.data.type, {
                 x:newX, y:newY
               });
