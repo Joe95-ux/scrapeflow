7b1c24cd81d35c88f15182f3c04b7f62137eb9e2
joe95-ux <ogorktabi@gmail.com>
2024-12-13 22:30:36 +0200

    highlight cards with invalid inputs

--- app/workflow/_components/nodes/NodeCard.tsx
+++ app/workflow/_components/nodes/NodeCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import useFlowValidation from "@/components/hooks/useFlowValidation";
 import { cn } from "@/lib/utils";
 import { useReactFlow } from "@xyflow/react";
 import { ReactNode } from "react";
@@ -25,12 +26,16 @@ function NodeCard({
     if (x === undefined || y === undefined) return;
     setCenter(x, y, { zoom: 1, duration: 500 });
   };
+
+  const {invalidInputs} = useFlowValidation();
+  const hasInvalidInputs = invalidInputs.some((node)=> node.nodeId === nodeId);
   return (
     <div
       onDoubleClick={handleDoubleClick}
       className={cn(
         "rounded-md cursor-pointer bg-background border-2 border-separate w-[420px] gap-2 flex flex-col",
-        isSelected && "border-primary"
+        isSelected && "border-primary",
+        hasInvalidInputs && "border-destructive border-2"
       )}
     >
       {children}
