7203c336b43fc1641dfb1fa4cd9934d36aeaafac
joe95-ux <ogorktabi@gmail.com>
2024-12-16 22:30:26 +0200

    complete runworkflow action

--- actions/workflows/runWorkflow.ts
+++ actions/workflows/runWorkflow.ts
@@ -2,48 +2,82 @@
 
 import prisma from "@/lib/prisma";
 import { FlowToExecutionPlan } from "@/lib/workflow/executionPlan";
+import { TaskRegistry } from "@/lib/workflow/task/Registry";
 import { WorkflowExecutionPlan } from "@/types/workflow";
 import { auth } from "@clerk/nextjs/server";
 
-export async function RunWorkflow(form:{workflowId: string, flowDefinition?: string}){
-    const {userId} = await auth();
+export async function RunWorkflow(form: {
+  workflowId: string;
+  flowDefinition?: string;
+}) {
+  const { userId } = await auth();
 
-    if(!userId){
-        throw new Error("unauthenticated!");
-    }
+  if (!userId) {
+    throw new Error("unauthenticated!");
+  }
 
-    const {workflowId, flowDefinition} =form;
-    if(!workflowId){
-        throw new Error("workflowId is required");
-    }
+  const { workflowId, flowDefinition } = form;
+  if (!workflowId) {
+    throw new Error("workflowId is required");
+  }
 
-    const workflow = await prisma.workflow.findUnique({
-        where:{
-            userId, id:workflowId,
-        }
-    })
+  const workflow = await prisma.workflow.findUnique({
+    where: {
+      userId,
+      id: workflowId,
+    },
+  });
 
-    if(!workflow){
-        throw new Error("workflow not found");
-    }
+  if (!workflow) {
+    throw new Error("workflow not found");
+  }
 
-    let executionPlan: WorkflowExecutionPlan;
-    if(!flowDefinition){
-        throw new Error("flow definition is not defined");
-    }
+  let executionPlan: WorkflowExecutionPlan;
+  if (!flowDefinition) {
+    throw new Error("flow definition is not defined");
+  }
 
-    const flow = JSON.parse(flowDefinition);
-    const result = FlowToExecutionPlan(flow.nodes, flow.edges);
+  const flow = JSON.parse(flowDefinition);
+  const result = FlowToExecutionPlan(flow.nodes, flow.edges);
 
-    if(result.error){
-        throw new Error("flow definition is not valid");;
-    }
+  if (result.error) {
+    throw new Error("flow definition is not valid");
+  }
 
-    if(!result.executionPlan){
-        throw new Error("no execution plan generated");
-    }
+  if (!result.executionPlan) {
+    throw new Error("no execution plan generated");
+  }
 
-    executionPlan = result.executionPlan;
-    console.log("Execution plan: ", executionPlan);
+  executionPlan = result.executionPlan;
+  const execution = await prisma.workflowExecution.create({
+    data: {
+      workflowId,
+      userId,
+      status: "PENDING",
+      startedAt: new Date(),
+      trigger: "manual",
+      phases: {
+        create: executionPlan.flatMap((phase) => {
+          return phase.nodes.flatMap((node) => {
+            return {
+                userId,
+                status: "CREATED",
+                number: phase.phase,
+                node: JSON.stringify(node),
+                name: TaskRegistry[node.data.type].label,
+            };
+          });
+        }),
+      },
+    },
+    select:{
+        id: true,
+        phases: true,
+    },
 
+  });
+
+  if(!execution){
+    throw new Error("workflow execution not created");
+  }
 }
