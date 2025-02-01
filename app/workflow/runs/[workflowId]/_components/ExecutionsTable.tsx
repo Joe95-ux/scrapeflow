67b841653a817ca1ee75b25eed8f09bc97ef711c
joe95-ux <ogorktabi@gmail.com>
2025-02-01 23:33:24 +0200

    add status indicator to phase details table

--- app/workflow/runs/[workflowId]/_components/ExecutionsTable.tsx
+++ app/workflow/runs/[workflowId]/_components/ExecutionsTable.tsx
@@ -10,6 +10,8 @@ import {
 } from "@/components/ui/table";
 import { DatesToDurationString } from "@/lib/helper/dates";
 import { Badge } from "@/components/ui/badge";
+import ExecutionStatusIndicator from "./ExecutionStatusIndicator";
+import { WorkflowExecutionStatus } from "@/types/workflow";
 
 type InitialDataType = Awaited<ReturnType<typeof GetWorkflowExecutions>>;
 
@@ -28,43 +30,45 @@ function ExecutionsTable({
   });
   return (
     <div className="border rounded-lg shadow-md overflow-auto">
-      <TableHeader className="bg-muted">
-        <TableRow>
-          <TableHead>Id</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Consumed</TableHead>
-          <TableHead className="text-right text-xs text-muted-foreground">
-            Started at (desc)
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody className="gap-2 h-full overflow-auto">
-        {query.data.map((execution) => {
-          const duration = DatesToDurationString(
-            execution.completedAt,
-            execution.startedAt
-          );
-          return (
-            <TableRow key={execution.id}>
-              <TableCell>
-                <div className="flex flex-col">
-                  <span className="font-semibold">{execution.id}</span>
-                  <div className="text-muted-foreground text-xs">
-                    <span>Triggered via</span>
-                    <Badge variant={"outline"}>{execution.trigger}</Badge>
+      <Table>
+        <TableHeader className="bg-muted">
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Consumed</TableHead>
+            <TableHead className="text-right text-xs text-muted-foreground">
+              Started at (desc)
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody className="gap-2 h-full overflow-auto">
+          {query.data.map((execution) => {
+            const duration = DatesToDurationString(
+              execution.completedAt,
+              execution.startedAt
+            );
+            return (
+              <TableRow key={execution.id}>
+                <TableCell>
+                  <div className="flex flex-col">
+                    <span className="font-semibold">{execution.id}</span>
+                    <div className="text-muted-foreground text-xs">
+                      <span>Triggered via</span>
+                      <Badge variant={"outline"}>{execution.trigger}</Badge>
+                    </div>
                   </div>
-                </div>
-              </TableCell>
-              <TableCell>
-                <div>
-                  <div>{execution.status}</div>
-                  <div>{duration}</div>
-                </div>
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
+                </TableCell>
+                <TableCell>
+                  <div>
+                    <div><ExecutionStatusIndicator status={execution.status as WorkflowExecutionStatus}/></div>
+                    <div>{duration}</div>
+                  </div>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
     </div>
   );
 }
