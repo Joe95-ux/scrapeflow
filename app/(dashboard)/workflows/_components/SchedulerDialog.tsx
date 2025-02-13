1e7d9b2289ba74c6bff53d4fd2d60e1325cfaca5
joe95-ux <ogorktabi@gmail.com>
2025-02-13 23:48:33 +0200

    finish scheduler dialog component

--- app/(dashboard)/workflows/_components/SchedulerDialog.tsx
+++ app/(dashboard)/workflows/_components/SchedulerDialog.tsx
@@ -1,9 +1,58 @@
 "use client";
 
-import React from 'react'
+import CustomDialogHeader from "@/components/CustomDialogHeader";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { CalendarIcon, TriangleAlertIcon } from "lucide-react";
 
 export default function SchedulerDialog() {
   return (
-    <div>SchedulerDialog</div>
-  )
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant={"link"}
+          size={"sm"}
+          className={cn("text-sm p-0 h-auto")}
+        >
+          <div className="flex items-center gap-1">
+            <TriangleAlertIcon className="h-3 w-3" />
+            Set schedule
+          </div>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="px-0">
+        <CustomDialogHeader
+          title="Schedule workflow execution"
+          icon={CalendarIcon}
+        />
+        <div className="p-6 space-y-4">
+          <p className="text-muted-foreground text-sm">
+            Specify a cron expression to schedule periodic workflow execution.
+            All times are in UTC
+          </p>
+          <Input placeholder="E.g. ****" />
+        </div>
+        <DialogFooter className="px-6 gap-2">
+          <DialogClose asChild>
+            <Button className="w-full" variant={"secondary"}>
+              Cancel
+            </Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button className="w-full">Save</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
