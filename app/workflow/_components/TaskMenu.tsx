611b8ffd907713ce387f402b84ea7718883a2629
joe95-ux <ogorktabi@gmail.com>
2025-03-09 23:35:12 +0200

    add fillinput task to task menu

--- app/workflow/_components/TaskMenu.tsx
+++ app/workflow/_components/TaskMenu.tsx
@@ -14,7 +14,15 @@ import React from "react";
 const TaskMenu = () => {
   return (
     <aside className="w-[340px] min-w-[340px] max-w-[340px] border-r-2 border-separate h-full p-2 px-4 overflow-auto">
-      <Accordion type="multiple" className="w-full" defaultValue={["extraction"]}>
+      <Accordion type="multiple" className="w-full" defaultValue={["extraction", "interactions"]}>
+      <AccordionItem value="interactions">
+          <AccordionTrigger className="font-bold">
+            User interactions
+          </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-1">
+            <TaskMenuBtn taskType={TaskType.FILL_INPUT} />
+          </AccordionContent>
+        </AccordionItem>
         <AccordionItem value="extraction">
           <AccordionTrigger className="font-bold">
             Data extraction
